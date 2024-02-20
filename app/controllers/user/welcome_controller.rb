ab11b05c541a6cccbbba4f24b76a0afa5e75a304
DESKTOP-8OS48M7\Robert <probbertt@gmail.com>
2024-02-20 23:45:13 +0200

    FII-8: - Implement login and register page.

--- app/controllers/user/welcome_controller.rb
+++ app/controllers/user/welcome_controller.rb
@@ -1,4 +1,5 @@
 class User::WelcomeController < UserApplicationController
   def index
+    clear_session
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope module: 'user', constraints: ->(req) { Context.get_context(req) == :user } do
     root to: 'welcome#index'
     match '/', to: 'welcome#index', via: :all
-    get '/login', to: 'sessions#login'
-    get '/register', to: 'sessions#register'
+    match '/login', to: 'sessions#login', via: %i[post get]
+    match '/register', to: 'sessions#register', via: %i[post get]
   end
 end
