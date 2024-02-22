796ed5399a158d2737d9e2670441b8416b357fda
atomicul <soaremihai2006@gmail.com>
2024-02-23 00:16:30 +0200

    Create basic navigation

--- app/controllers/user/welcome_controller.rb
+++ app/controllers/user/welcome_controller.rb
@@ -1,5 +1,18 @@
 class User::WelcomeController < UserApplicationController
   def index
+  end
+
+  def search
     clear_session
   end
+
+  def scan
+  end
+
+  def hub
+  end
+
+  def profile
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   scope module: 'user', constraints: ->(req) { Context.get_context(req) == :user } do
     root to: 'welcome#index'
     match '/', to: 'welcome#index', via: :all
+    match '/search', to: 'welcome#search', via: :all
+    match '/scan', to: 'welcome#scan', via: :all
+    match '/hub', to: 'welcome#hub', via: :all
+    match '/profile', to: 'welcome#profile', via: :all
     match '/login', to: 'sessions#login', via: %i[post get]
     match '/register', to: 'sessions#register', via: %i[post get]
   end
