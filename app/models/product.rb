256afc2786509979b83a9077bd664ec357b7d247
atomicul <soaremihai2006@gmail.com>
2024-02-25 17:18:10 +0200

    Exctract logic to separate class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
-require "http"
-
 class Product
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -43,34 +41,8 @@ class Product
   field :approved, type: Boolean
   field :rating, type: Float
 
-  def self.fromOpenFoodFacts(ean)
-    endpoint = "https://world.openfoodfacts.org/api/v3/product/#{ean}".freeze
-    response = HTTP.get(endpoint)
-    unless response.status.success?
-      return nil
-    end
-
-    data = response.parse[:product.to_s]
-
-    allergens = data[:allergens_tags.to_s].map { |a| a.gsub("en:", "") }
-    allergens.map! { |a| Allergen.where(off_counterpart: a).first }.filter! { |a| not a.nil? }
-
-    Product.new(
-      ean: ean,
-      brand: data[:brands.to_s],
-      name: data[:product_name.to_s],
-      allergens: allergens.map { |a| a._id },
-      weight: data[:product_quantity.to_s],
-      serving_quantity: data[:serving_quantity.to_s],
-      calories: data[:nutriments.to_s]["energy-kcal_100g"],
-      protein: data[:nutriments.to_s]["proteins_100g"],
-      fat: data[:nutriments.to_s]["fat_100g"],
-      saturated_fat: data[:nutriments.to_s]["saturated_fat_100g"],
-      carbohydrates: data[:nutriments.to_s]["carbohydrates_100g"],
-      fiber: data[:nutriments.to_s]["fiber_100g"],
-      sugar: data[:nutriments.to_s]["sugars_100g"],
-      sodium: data[:nutriments.to_s]["sodium_100g"],
-    )
+  def self.from_open_food_facts(ean)
+    OpenFoodFacts.product(ean)
   end
 
 end
