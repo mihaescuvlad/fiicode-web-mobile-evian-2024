567b7a6bbadb2f9c79e292bad820653cc44be1f6
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-02-29 11:55:32 +0200

    Review Controller changes

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -1,6 +1,8 @@
 class User::ReviewsController < UserApplicationController
   before_action :authenticate_user!
-  before_action :set_review, only: %i[ show edit update destroy ]
+  before_action :set_product
+  before_action :set_review, only: %i[edit update destroy]
+  before_action :review_params, only: %i[edit update destroy]
 
   def index
     @reviews = Review.all
@@ -13,23 +15,37 @@ class User::ReviewsController < UserApplicationController
   end
 
   def create
-    @review = Review.new(review_params)
+    begin 
+      @review = Review.new(review_params)
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to user_product_path(@review.product_id), notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @review.save
+          format.html { redirect_to user_product_path(@review.product_id), notice: 'Review was successfully created.' }
+          format.json { render :show, status: :created, location: @review }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @review.errors, status: :unprocessable_entity }
+        end
       end
+    rescue
+      render json: { message: "You have to click on the \"Dislike\" or \"Like\" buttons for the product." }, status: :bad_request and return
     end
   end
 
   def update
-    puts params
-
-    render json: { message: "Not implemented" }, status: :bad_request and return
+    begin
+      respond_to do |format|
+        if review.update(review_params)
+          format.html { redirect_to user_product_path(@review.product_id), notice: "Review was successfully updated." }
+          format.json { render :show, status: :ok, location: @review }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @review.errors, status: :unprocessable_entity }
+        end
+      end
+    rescue
+      render json: { message: "Something went wrong with updating your review. Please take a break, read a book and try again later." }, status: :bad_request and return
+    end
   end
 
   def destroy
@@ -43,27 +59,24 @@ class User::ReviewsController < UserApplicationController
 
   private
 
+  def set_product
+    @product ||= Product.find(params[:product_id])
+  end
+
   def set_review
-    @review = Review.find(params[:id])
+    @review ||= Review.find_by(id: params[:id], product_id: @product.id, reviewer_id: current_user.id)
   end
 
   def review_params
-    login_id = session[:login_id]["$oid"]
-    user_id = User.find_by(login_id: login_id).id
+    raise "You have to like or dislike a product." if params[:rating] == nil
+    rating = params[:rating] == "like" ? true : false
 
-    product_id = params[:product_id]
-
-    review_attributes = params.require(:review).permit(:rating, :comment)
-    rating = review_attributes[:rating] == "true" ? true : false
-
-    review_params = {
+    {
+      reviewer_id: current_user.id,
+      product_id: @product.id,
       rating: rating,
-      comment: review_attributes[:comment],
-      reviewer_id: user_id,
-      product_id: product_id,
+      comment: params[:comment],
       helpful_votes: 0
     }
-
-    review_params
   end
 end
