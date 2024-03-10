02d438130b440c8f9633352957fa7884e9d7a2b1
PopaRobert <95902235+PopaRobert@users.noreply.github.com>
2024-03-10 18:29:03 +0200

    Feature/fii 26 (#28)
    * FII-39: - Add a comparison side by side for the submitted products.
    
    * FII-39: - Add the nutriscore of the product.
    
    * FII-26: - Enable the posibility to add a product to favorites.

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -9,7 +9,7 @@ module CardsHelper
         
         concat(content_tag(:button, type: 'button', class: '!absolute top-0 right-0 h-8 w-8 select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-red-500 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none max-h-[32px] max-w-[32px] hover:bg-red-500/10 active:bg-red-500/30') do
           concat(content_tag(:span, class: '-translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2') do
-            concat(content_tag(:i, '', class: 'mdi mdi-heart', style: 'font-size: 1.5rem;'))
+            concat(favorite_product_icon(product))
           end)
         end)
 
@@ -67,4 +67,13 @@ module CardsHelper
       "N/A"
     end
   end
+
+  def favorite_product_icon(product)
+    if current_user.favorites.include?(product.id)
+      content_tag(:i, '', class: 'mdi mdi-heart', style: 'font-size: 1.5rem;', id: "heart-icon-#{product.id}")
+    else
+      content_tag(:i, '', class: 'mdi mdi-heart-outline', style: 'font-size: 1.5rem;', id: "heart-icon-#{product.id}")
+    end
+  end
+
 end
