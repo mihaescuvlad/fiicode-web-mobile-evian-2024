6c376264aebfb3152066c61ca9df0d1aa2fb54d7
Mihaescu Vlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-03-10 20:05:08 +0200

    Add Product Recommendation ML API (#29)
    * Add Product Recommendation API
    
    * Update API to support favorites

---
+++ recommendation_api/src/objectid.py
@@ -0,0 +1,35 @@
+from fastapi.encoders import jsonable_encoder
+from bson import ObjectId
+from pydantic.json import ENCODERS_BY_TYPE
+
+
+class PydanticObjectId(ObjectId):
+    """
+    Object Id field. Compatible with Pydantic.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return PydanticObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: dict):
+        field_schema.update(
+            type="string",
+        )
+    
+    def to_json(self):
+        return jsonable_encoder(self, exclude_none=True)
+
+    def to_bson(self):
+        data = self.dict(by_alias=True, exclude_none=True)
+        if data.get("_id") is None:
+            data.pop("_id", None)
+        return data
+
+
+ENCODERS_BY_TYPE[PydanticObjectId] = str
