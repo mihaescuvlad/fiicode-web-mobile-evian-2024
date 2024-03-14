fbf60e7e1b67519b12b22a28e67287ad11f19dac
atomicul <soaremihai2006@gmail.com>
2024-03-14 23:43:32 +0200

    Implement react-rails gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,8 @@ import '../src/notifier/error_notifier'
 import '../src/notifier/success_notifier'
 import '../src/form/form_controller'
 import '../src/user/hub/posts/post'
+import ReactRailsUJS from 'react_ujs';
+
+const componentRequireContext = require.context("components", true);
+
+ReactRailsUJS.useContext(componentRequireContext);
