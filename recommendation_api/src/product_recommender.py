a5a51f2add7a284330adb36a72d6e3efc137cdc6
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-03-15 13:39:20 +0200

    Add Dietary Preference Penalty

--- recommendation_api/src/product_recommender.py
+++ recommendation_api/src/product_recommender.py
@@ -1,7 +1,7 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
 
-def compute_similarity(product1, product2, user_allergens = []):
+def compute_similarity(product1, product2, user_allergens=[], user_dietary_preferences=[]):
     scaler = MinMaxScaler()
     attributes1 = scaler.fit_transform([[product1.calories, product1.fat, product1.carbohydrates, product1.protein]])
     attributes2 = scaler.transform([[product2.calories, product2.fat, product2.carbohydrates, product2.protein]])
@@ -23,12 +23,17 @@ def compute_similarity(product1, product2, user_allergens = []):
     if any(allergen in product2.allergens for allergen in user_allergens):
         allergen_penalty = 0.035
 
+    dietary_penalty = 0.0
+    if "VEGAN" in user_dietary_preferences and not product2.vegan:
+        dietary_penalty += 0.01
+    if "VEGETARIAN" in user_dietary_preferences and not product2.vegetarian:
+        dietary_penalty += 0.01
 
-    similarity_score = 0.7 * numeric_similarity + 0.3 * ingredients_similarity - allergen_penalty
+    similarity_score = 0.7 * numeric_similarity + 0.3 * ingredients_similarity - allergen_penalty - dietary_penalty
     return similarity_score
 
 
-def recommend_products(user_basket, all_products, user_allergens=[], N=9):
+def recommend_products(user_basket, all_products, user_allergens=[], user_dietary_preferences=[], N=9):
     if not user_basket:
         return sorted(all_products, key=lambda x: x.rating, reverse=True)
 
@@ -36,7 +41,7 @@ def recommend_products(user_basket, all_products, user_allergens=[], N=9):
     recommendations = []
 
     for available_product in available_products:
-        similarity_scores = [compute_similarity(user_product, available_product, user_allergens) for user_product in user_basket]
+        similarity_scores = [compute_similarity(user_product, available_product, user_allergens, user_dietary_preferences) for user_product in user_basket]
         average_similarity = sum(similarity_scores) / len(similarity_scores)
         
         recommendations.append((available_product, average_similarity))
