9d37904316d29e3c3687eb6fb758bda16c8373ad
Robert <probbertt@gmail.com>
2024-03-16 15:31:02 +0200

    FII-94: - Fix logic issue.

--- app/controllers/admin/feedback_messages_controller.rb
+++ app/controllers/admin/feedback_messages_controller.rb
@@ -3,7 +3,11 @@ class Admin::FeedbackMessagesController < AdminApplicationController
 
   def index
     params[:page] ||= 1
-    @total_pages = FeedbackMessage.where(read: false).count / 10 + 1
+    total_items = FeedbackMessage.where(read: false).count
+    items_per_page = 10
+    total_pages = (total_items.to_f / items_per_page).ceil
+    @total_pages = total_pages > 0 ? total_pages : 1
+
     @feedback_messages = FeedbackMessage.where(read: false).order(created_at: :asc).skip((params[:page].to_i - 1) * 10).limit(10)
   end
 
