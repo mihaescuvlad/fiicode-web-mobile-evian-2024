271d92a429733dd947b6220340e5a7944d92c316
Popa Robert-Andrei <130554714+probbertt@users.noreply.github.com>
2024-03-18 21:52:16 +0200

    FII-101: - Add products page. (#42)
    Co-authored-by: Robert <probbertt@gmail.com>

--- app/controllers/user/baskets_controller.rb
+++ app/controllers/user/baskets_controller.rb
@@ -3,10 +3,19 @@ class User::BasketsController < UserApplicationController
 
   def show
     params[:page] ||= 1
-    recommendation_products = RecommendationsApi.paginated_products(current_user, params[:page]) || {}
+    params[:favorites_page] ||= 1
+    recommendation_products = RecommendationsApi.paginated_products(current_user, params[:page], 9) || {}
     @products = Product.where(:_id.in => recommendation_products["products"])
-    basket_product_ids = current_user.favorites
-    @basket_products = Product.where(:_id.in => basket_product_ids)
     @total_pages = recommendation_products["total_pages"]
+    
+    basket_product_ids = current_user.favorites
+    favorite_total_pages = (basket_product_ids.count.to_f / 9).ceil
+
+    if params[:favorites_page].to_i > favorite_total_pages
+      params[:favorites_page] = favorite_total_pages
+    end
+
+    @basket_products = Product.where(:_id.in => basket_product_ids).skip((params[:favorites_page].to_i - 1) * 9).limit(9)
+    @basket_total_pages = favorite_total_pages || 1
   end
 end
