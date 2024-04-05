8ecdbe09519a8e70dd60c46873172f54004b7e67
atomicul <soaremihai2006@gmail.com>
2024-04-05 03:10:49 +0300

    Revert to letter opener gem in dev environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,15 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'www.freshly.local',
+    user_name: Rails.application.credentials.dig(:gmail, :username),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
