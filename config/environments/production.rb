84fd191a61799f1de70af9c55248fb4434cfe51a
Robert <popa.robert.s6c@student.ucv.ro>
2024-04-05 11:56:17 +0300

    Fix mailing issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,16 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: "www.freshly.local" }
+  config.action_mailer.default_url_options = { host: "freshly-o212.onrender.com" }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'freshly-o212.onrender.com',
+    user_name: Rails.application.credentials.dig(:gmail, :username),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
