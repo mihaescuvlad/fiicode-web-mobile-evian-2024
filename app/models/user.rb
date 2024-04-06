f4abcf82374800cbd2a848cb3c1413c22a93df47
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-04-06 22:35:07 +0300

    Add Total Macros computation

--- app/models/user.rb
+++ app/models/user.rb
@@ -136,4 +136,40 @@ class User
   def diary_products(date = Date.today)
     Diary.where(user_id: self.id).and("products.#{date.strftime('%Y-%m-%d')}".to_sym.exists => true).first.try(:products).try(:[], date.strftime('%Y-%m-%d')) || []
   end
+
+  def get_nutritional_targets
+    @calorie_target = 2000
+
+    unless self.height.nil?
+      @calorie_target = self.height * 2.2 * 14
+    end
+    @protein_target = @calorie_target * 0.3 / 4
+    @carbs_target = @calorie_target * 0.4 / 4
+    @fat_target = @calorie_target * 0.3 / 9
+  end
+
+  def get_nutritional_stats
+    get_nutritional_targets
+  
+    @calorie_eaten = 0
+    @protein_eaten = 0
+    @carbs_eaten = 0
+    @fat_eaten = 0
+  
+    fetched_diary_products = diary_products
+
+    puts fetched_diary_products.inspect
+  
+    if fetched_diary_products.present?
+      fetched_diary_products.each do |diary_product|
+        puts diary_product.inspect
+        product = Product.find_by(_id: diary_product["product_id"])
+  
+        @calorie_eaten += (product.calories / 100 * diary_product["quantity"]).round(2)
+        @protein_eaten += (product.protein / 100 * diary_product["quantity"]).round(2)
+        @carbs_eaten += (product.carbohydrates / 100 * diary_product["quantity"]).round(2)
+        @fat_eaten += (product.fat / 100 * diary_product["quantity"]).round(2)
+      end
+    end
+  end  
 end
